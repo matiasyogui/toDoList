a1aedb060b170903fed67031a6fe57b8cfe3ae10
Matias Yogui <matiasyogui@hotmail.com>
2021-02-27 01:57:29 -0300

    las tasks tachadas se quedan tachadas al refresacar la pagina

--- main.js
+++ main.js
@@ -1,43 +1,24 @@
+//-------------------VAR GLOBALES------------
 const $aceptar = document.querySelector('#aceptar');
 const $input = document.querySelector('input');
-
-if(!localStorage.getItem('tasks')){
-    localStorage.setItem('tasks', 0);
-}
-
 let arrLocalStorage;
 
-if(JSON.parse(localStorage.getItem('tasks') != 0)){
-    arrLocalStorage = JSON.parse(localStorage.getItem('tasks'));
-}else{
-    arrLocalStorage = [];
+//-------------------FUNCIONES------------
+
+function habilitarBoton() {
+    $aceptar.disabled = false;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log(arrLocalStorage);
-
-    if(arrLocalStorage != 0){
-        arrLocalStorage.forEach(task => {
-            agregarALista(task[0]);
-        });
-    }
-});
-
-$aceptar.addEventListener('click', () => {
-    const texto = $input.value;
-    agregarALista(texto);
-    crearTask(texto);
-    borrarInput();
-});
+function desabilitarBoton() {
+    $aceptar.disabled = true;
+}
 
 function borrarInput() {
     $input.value = '';
 }
 
-function agregarALista(texto) {
-    if(texto == ''){
-        return;
-    }
+
+function agregarALista(texto, tachado) {
 
     const $items = document.querySelector('#items');
 
@@ -52,6 +33,7 @@ function agregarALista(texto) {
     const $itemCheck = document.createElement('input');
     $itemCheck.type = 'checkbox';
     $itemCheck.id = 'item-check';
+    
 
     const $itemTexto = document.createElement('p');
     $itemTexto.classList.add('parrafo');
@@ -62,13 +44,19 @@ function agregarALista(texto) {
     $eliminar.id = 'eliminar';
     $eliminar.textContent = 'ELIMINAR';
 
+    if(tachado){
+        $itemCheck.checked = true;
+        $itemTexto.classList.add('tachado');
+    }
+
     $item.appendChild($itemCheck);
     $item.appendChild($itemTexto);
     $item.appendChild($eliminar);
 
     $items.appendChild($item);
-    
+
     console.log('Se agrego a la lista ' + texto);
+    desabilitarBoton();
 }
 
 function crearTask(texto){
@@ -88,35 +76,38 @@ function guardarLocalStorage(arr){
     localStorage.setItem('tasks', JSON.stringify(arr))
 }
 
-document.addEventListener('click', e => {
-    if(e.target.matches('#item-check')){
-        console.log(e.target.checked);
-        console.log(document.querySelectorAll('#item-check'));
-    
-        let parrafo = e.target.nextElementSibling;
-    
-        if(e.target.checked){
-            parrafo.classList.add('tachado');
-            console.log(parrafo.textContent + ' fue tachado.');
-        }else{
-            parrafo.classList.remove('tachado');
-            console.log(parrafo.textContent + ' fue destachado.');
-        }
-        
-        console.log(e);
+
+//-------------------EVENT LISTENERS------------
+
+document.addEventListener('DOMContentLoaded', () => { //DOMContentLoaded, cuando se carga el html
+    if(!localStorage.getItem('tasks')){
+        localStorage.setItem('tasks', 0);
     }
+    
+    if(JSON.parse(localStorage.getItem('tasks') != 0)){
+        arrLocalStorage = JSON.parse(localStorage.getItem('tasks'));
+    }else{
+        arrLocalStorage = [];
+    }
+    
+    console.log(arrLocalStorage);
+    desabilitarBoton();
 
-    if(e.target.matches("#eliminar")){
-        e.target.parentElement.remove();
-        console.log(document.querySelector('#items').children);
-
-        if(document.querySelector('#items').children.length == 0){
-            document.querySelector('#items').className = 'oculto';
-        }
+    if(arrLocalStorage != 0){
+        arrLocalStorage.forEach(task => {
+            agregarALista(task[0], task[1]);
+        });
     }
 });
 
-document.querySelector('#reset').addEventListener('mouseup', () => {
+$aceptar.addEventListener('click', () => { //BOTON ACEPTAR
+    const texto = $input.value;
+    agregarALista(texto);
+    crearTask(texto);
+    borrarInput();
+});
+
+document.querySelector('#reset').addEventListener('mouseup', () => { //BOTON RESET
     let items = document.querySelectorAll('#item');
     console.log(items);
 
@@ -128,4 +119,50 @@ document.querySelector('#reset').addEventListener('mouseup', () => {
     localStorage.setItem('tasks', 0);
 
     document.querySelector('#items').className = 'oculto';
+});
+
+document.addEventListener('click', e => { //CHECKBOX | BOTON ELIMINAR
+    if(e.target.matches('#item-check')){ //CHECKBOX
+        console.log(document.querySelectorAll('#item-check'));
+    
+        let parrafo = e.target.nextElementSibling;
+        console.log(parrafo);
+        let arrTasks = JSON.parse(localStorage.getItem('tasks'));
+
+        for(let i = 0; i < arrTasks.length; i++){
+            if(arrTasks[i][0] == parrafo.textContent){
+                if(e.target.checked){
+                    parrafo.classList.add('tachado');
+                    arrTasks[i][1] = true;
+                    console.log(arrTasks[i]);
+                    console.log(parrafo.textContent + ' fue tachado.');
+                }else{
+                    parrafo.classList.remove('tachado');
+                    arrTasks[i][1] = false;
+                    console.log(arrTasks[i]);
+                    console.log(parrafo.textContent + ' fue destachado.');
+                }
+            }
+        }
+        guardarLocalStorage(arrTasks);
+        console.log(e);
+    }
+
+    if(e.target.matches("#eliminar")){ //ELIMINAR
+        e.target.parentElement.remove();
+        console.log(document.querySelector('#items').children);
+
+        if(document.querySelector('#items').children.length == 0){
+            document.querySelector('#items').className = 'oculto';
+        }
+    }
+});
+
+
+$input.addEventListener('input', (e) => { //INPUT, HABILITAR | DESABILITAR BOTON ACEPTAR
+    if(e.target.value == ''){
+        desabilitarBoton();
+    }else{
+        habilitarBoton();
+    }
 });
